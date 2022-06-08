29c9fb8c1d79fdb4b1fe9ce43a7685664956442b
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-06-07 18:59:51 -0700

    Challenge solved!

--- src/LastDigitChecker.java
+++ src/LastDigitChecker.java
@@ -1,2 +1,27 @@
 public class LastDigitChecker {
+    public static boolean hasSameLastDigit(int one, int two, int three){
+        if (!isValid(one) || !isValid(two) || !isValid(three)){
+            return false;
+        }
+        else{
+            int first = one % 10;
+            int second = two % 10;
+            int third = three % 10;
+            if (first == second || first == third || second == third){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    public static boolean isValid(int number){
+        if (number < 10 || number > 1000){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@
 Write a method named hasSameLastDigit with three parameters of type int
 each number should be within the range or 10 to 1000 (inclusive). If any number is out of range, return false.
 Return true if at least two numbers share the rightmost digit, otherwise return false.
+
+ Write another method named isValid with one parameter of type int.
+ the method needs to return true if the number parameter is in range of 10 - 1000 inclusive, otherwise return false.
  */
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(LastDigitChecker.hasSameLastDigit(123, 456,783));
 
     }
 }
