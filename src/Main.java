9d51f94ad9b0b1b68659d5f38887afe07adbf00e
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-06-07 18:49:22 -0700

    Created framework and detailed challenge in main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*
+Write a method named hasSameLastDigit with three parameters of type int
+each number should be within the range or 10 to 1000 (inclusive). If any number is out of range, return false.
+Return true if at least two numbers share the rightmost digit, otherwise return false.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
